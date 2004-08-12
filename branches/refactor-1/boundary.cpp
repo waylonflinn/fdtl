75cb941d6d8f96635a47b4adbbceb0b2544abb41
ewf <ewf@339b2cbf-344d-0410-bec1-b5a737de3891>
2004-08-12 23:05:46 +0000

    creating branch for the major restructuring project

---
+++ branches/refactor-1/boundary.cpp
@@ -0,0 +1,51 @@
+#include <vector>
+#include <iterator>
+/* boundary, class describing boundary conditions for a finite differenced pde
+ *
+ */
+
+using std::vector;
+
+class boundary
+{
+public:
+  // constants
+  const static int DIRICHLET = 0;
+  const static int NEUMANN = 1;
+
+  // methods
+  int type() const { return _type; }	// type of boundary conditions
+  const double& value(int i);
+  const double& operator[](int i);
+
+  // constructors
+  boundary(int type,
+	   vector<double>::iterator start,
+	   vector<double>::iterator end);
+
+private:
+  int _type;
+  vector<double> _value;
+
+};
+
+/* create a boundary condition of the given type with the values returned
+ * by the given iterator pair.
+ */
+boundary::boundary(int type,
+		   vector<double>::iterator start,
+		   vector<double>::iterator end)
+{
+  _value = vector<double>(start, end);
+  _type = type;
+}
+
+/* get the value at position i (zero based)
+ */
+const double& boundary::value(int i)
+{ return _value[i]; }
+
+/* subscript ([]) operator (synonym for value(int))
+ */
+const double& boundary::operator[](int i) 
+{ return _value[i]; }
