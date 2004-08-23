70861604652710c89c22a927dab61a4ea62e75d3
ewf <ewf@339b2cbf-344d-0410-bec1-b5a737de3891>
2004-08-23 17:13:07 +0000

    added overrelaxation and chebyshev acceleration to sor.

--- branches/refactor-1/algorithm/successive_overrelaxation.cpp
+++ branches/refactor-1/algorithm/successive_overrelaxation.cpp
@@ -16,23 +16,27 @@ int successive_overrelaxation::solve (problem& prob, goal& g)
   int J = prob.J();
   int i, j;
   int oe = 0;		// odd/even marker
-  int is = 1, js =1;	// i and j starting indeces
+  int is = 1, os = 1;	// inner and outer starting point trackers
 
   while(!g(prob) && (count < (*this).cutoff)){
-    for(oe = 0; oe < 2; ++oe){
-      for(i = is; i < I; i++){
-	  for(j = js; j < J; j+=2){
-	      res = (prob.e(i,j)*prob.at(i,j) +
-		     (prob.a(i,j)*prob.at(i-1,j) + prob.b(i,j)*prob.at(i+1,j) +
-		     prob.c(i,j)*prob.at(i,j-1) + prob.d(i,j)*prob.at(i,j+1)) +
-		     prob.f(i,j));
-	      
-	      // set value
-	      prob.u(i, j) = (prob.u(i, j) - res/prob.e(i,j));
-	  }
-	  js=3-js;	// 1 or 2
+    for(oe = 0; oe < 2; ++oe){	// odd and even passes
+      for(i = 1; i < I; i++){
+	is = os;
+	for(j = is; j < J; j+=2){
+	  res = (prob.e(i,j)*prob.at(i,j) +
+		 (prob.a(i,j)*prob.at(i-1,j) + prob.b(i,j)*prob.at(i+1,j) +
+		  prob.c(i,j)*prob.at(i,j-1) + prob.d(i,j)*prob.at(i,j+1)) +
+		 prob.f(i,j));
+	  
+	  // set value
+	  prob.u(i, j) -= param*res/prob.e(i,j);
+	}
+	is=3-is;	// 1 or 2
       }
-      is=3-is;		// 1 or 2
+      os=3-os;		// 1 or 2
+      param = (count == 1 && oe == 0 ? 
+	       1.0/(1.0 - 0.5*rad*rad) :
+	       1.0/(1.0 - 0.25*rad*rad*param));
     }
     ++count;
   }
