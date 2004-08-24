7d4cc3b7ca9c4a1f6851963fe709721c59689df6
Waylon Flinn <waylonflinn@users.noreply.github.com>
2004-08-24 17:15:47 +0000

    added code for processing first few command line args. added an interface testing program. created sample input file.

--- interface/interface.h
+++ interface/interface.h
@@ -1,13 +1,19 @@
+#include <cerrno>
+#include <cstdlib>
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
 #include <utility>
 #include "command_line.h"
 
 using std::cout;
+using std::cerr;
 using std::endl;
 using std::vector;
 using std::ostream;
+using std::ofstream;
+using std::ifstream;
 
 class interface
 {
@@ -19,38 +25,43 @@ class interface
   static const pair<double, double> DEF_X;
   static const pair<double, double> DEF_Y;
 
-  static const command_line CL;
-
-  // constructors
-  interface(string id, int argc, char* argv[]);
-
-  // methods
-  int I();
-  int J();
-  pair<double, double> x();
-  pair<double, double> y();
-  ostream& output();
-  vector< vector<double> > boundary();
-
- protected:
-  // constants
-  static const string NAME;
-  static const argument INPUT;
+  /* these constants represent the command line for this interface */
+  static const string NAME;	// name of the program
+  static const argument INPUT;	// input file
   static const option OPT_I;
   static const option OPT_J;
   static const option OPT_X;
   static const option OPT_Y;
 
+  // constructors
+  interface(string id, int argc, char* argv[]);
+  interface();
+
+  // methods
+  int I() const { return gx; }		// grid points in first var
+  int J() const { return gy; }		// grid points in second var
+  pair<double, double> x() const { return rx; }	// range of first var
+  pair<double, double> y() const { return ry; }	// range of second var
+  ostream& output();	// output
+  vector< vector<double> > boundary();
+
+ protected:
+  // constants
   static const string FIRST_VAR;
   static const string SEC_VAR;
-  const static argument ARR_X[];
-  const static argument ARR_Y[];
-  const static option ARR_OPT[];
+  static const argument ARR_X[];
+  static const argument ARR_Y[];
+  static const option ARR_OPT[];
 
   // variables
+  command_line cl;
   int gx;
   int gy;
   pair<double, double> rx;
   pair<double, double> ry;
-  ostream out;
+
+  // methods
+  int grid_opt(char);
+  pair<double, double> range_opt(char);
+
 };
