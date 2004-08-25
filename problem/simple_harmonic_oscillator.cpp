7394bf1fa1c570fda216ed8ba7cd2a384daa5adc
Waylon Flinn <waylonflinn@users.noreply.github.com>
2004-08-25 13:46:06 +0000

    added another intermediate to problem_basic and laplace. created simple_harmonic_oscillator (sho) class.

---
+++ problem/simple_harmonic_oscillator.cpp
@@ -0,0 +1,20 @@
+#include "simple_harmonic_oscillator.h"
+
+simple_harmonic_oscillator::simple_harmonic_oscillator(
+	  int I,
+	  int J,
+	  pair<double, double> range_x,
+	  pair<double, double> range_y,
+	  const boundary& top,		// boundary objects for each boundary
+	  const boundary& right,
+	  const boundary& bottom,
+	  const boundary& left,
+	  double coeff_x,
+	  double coeff_y,
+	  double eigenvalue)
+  : problem_basic(I, J, range_x, range_y, top, right, bottom, left),
+    cx(coeff_x), cy(coeff_y), eig(eigenvalue), mcx(ssx*cx), mcy(ssy*cy),
+    meig(ssx*eig)
+        
+{
+}
--- problem/simple_harmonic_oscillator.h
+++ problem/simple_harmonic_oscillator.h
@@ -4,26 +4,40 @@
 
 #include "problem_basic.h"
 
-class laplace : public problem_basic
+class simple_harmonic_oscillator : public problem_basic
 {
 public:
 
   // constructors
-  laplace(int I,
+  simple_harmonic_oscillator(
+	  int I,
 	  int J,
 	  pair<double, double> range_x,
 	  pair<double, double> range_y,
 	  const boundary& top,		// boundary objects for each boundary
 	  const boundary& right,
 	  const boundary& bottom,
-	  const boundary& left);
+	  const boundary& left,
+	  double coeff_x,
+	  double coeff_y,
+	  double eigenvalue);
 
   // methods
   double a(int i, int j) const { return 1;}
   double b(int i, int j) const { return 1;}
   double c(int i, int j) const { return rss;}
   double d(int i, int j) const { return rss;}
-  double e(int i, int j) const { return ast;}
+  double e(int i, int j) const 
+    { return (mcx*i*i*ssx)+(mcy*j*j*ssy)- meig - ast;}
   double f(int i, int j) const { return 0;}
+
+ private:
+  double cx;
+  double cy;
+  double eig;
+  // intermediates for e(int,int)
+  double mcx;	// ssx*cx
+  double mcy;	// ssx*cy
+  double meig;	// ssx*eig
 
 };
