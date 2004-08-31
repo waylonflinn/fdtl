f81994e6f1bf3a3662d5add83ff5c7202b725d12
ewf <ewf@339b2cbf-344d-0410-bec1-b5a737de3891>
2004-08-31 20:55:33 +0000

    changed sho definition, added a missing factor of 2 to some of the potential terms in e(int,int).

--- trunk/problem/simple_harmonic_oscillator.cpp
+++ trunk/problem/simple_harmonic_oscillator.cpp
@@ -14,11 +14,11 @@ simple_harmonic_oscillator::simple_harmonic_oscillator(
 	  double eigenvalue)
   : problem_basic(I, J, range_x, range_y, top, right, bottom, left),
     cx(coeff_x), cy(coeff_y), eig(eigenvalue),
-    p1x(-1*cx*ssx*x.first*x.first), p2x(-1*cx*ssx*2*x.first*sx),
-    p3x(-1*cx*ssx*ssx),
-    p1y(-1*cy*ssx*y.first*y.first), p2y(-1*cy*ssx*2*sy*y.first),
-    p3y(-1*cy*ssx*ssy),
-    meig(ssx*eigenvalue)
+    p1x(-2*cx*ssx*x.first*x.first), p2x(-2*cx*ssx*2*x.first*sx),
+    p3x(-2*cx*ssx*ssx),
+    p1y(-2*cy*ssx*y.first*y.first), p2y(-2*cy*ssx*2*sy*y.first),
+    p3y(-2*cy*ssx*ssy),
+    meig(2*ssx*eigenvalue)
         
 {
   double norm_av = 1/((x.second-x.first)*(y.second-y.first));
