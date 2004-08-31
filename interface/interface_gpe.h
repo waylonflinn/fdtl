f49f597897b6e0cf6d47ac2b87623297b11405fe
Waylon Flinn <waylonflinn@users.noreply.github.com>
2004-08-31 02:17:27 +0000

    finished and debugged interface for gross_pitaevskii (gpe).

--- interface/interface_gpe.h
+++ interface/interface_gpe.h
@@ -1,28 +1,30 @@
-#ifndef PDE_INTERFACE_SHO
-#define PDE_INTERFACE_SHO
+#ifndef PDE_INTERFACE_GPE
+#define PDE_INTERFACE_GPE
 
 #include "interface.h"
-#include "option_set_sho.h"
+#include "option_set_gpe.h"
 
-class interface_sho : public interface
+class interface_gpe : public interface
 {
  public:
 
   // constants
   static const double DEF_A;
   static const double DEF_B;
-  static const double DEF_EIG;
+  static const double DEF_PARM;
   static const pair<double,double> DEF_X;
   static const pair<double,double> DEF_Y;
 
   // constructors
-  interface_sho(string id, int argc, char* argv[]);
-  interface_sho();
+  interface_gpe(string id, int argc, char* argv[]);
+  interface_gpe();
 
   // methods
   double a() const { return cx; }
   double b() const { return cy; }
+  bool eigenvalue_present() const { return eig_pres; }
   double eigenvalue() const { return eig; }
+  double parameter() const { return parm; }
 
  protected:
 
@@ -30,9 +32,11 @@ class interface_sho : public interface
   double cx;
   double cy;
   double eig;
+  double parm;
+  bool eig_pres;
 
   // methods
 
 };
 
-#endif	// PDE_INTERFACE_SHO
+#endif	// PDE_INTERFACE_GPE
