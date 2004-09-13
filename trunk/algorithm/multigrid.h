263b0deb5cdd5b730d5d0f654fd8aa4e8709fa24
ewf <ewf@339b2cbf-344d-0410-bec1-b5a737de3891>
2004-09-13 21:22:35 +0000

    implemented multigrid class. did initial debugging and testing. added TODO list.

--- trunk/algorithm/multigrid.h
+++ trunk/algorithm/multigrid.h
@@ -1,38 +1,70 @@
-/* an adaptive linear multigrid system which uses nested iteration with
+/* a very simple linear multigrid system which uses nested iteration with
  * bilinear interpolation for the prolongation operator and the standard
  * half weighting operator for restriction.
- * recursively applies a smoother to a problem on a series of coarser
- * grids until the goal is met. this process starts on a course
- * grid and is repreated on a series of finer grids, using the recursive
- * coarse solutions to constuct corrections, until the specified fineness
- * is reached.
+ * applies the smoother to the problem, beggining on the smallest grid, until
+ * the goal is reached then prolongs the solution to next finer grid until
+ * the desired grid size is reached.
+ * boundary conditions for each grid are generated from those of the problem
+ * using the restriction operator.
  */
 #include "gauss_seidel.h"
-#include "solver_basic.h"
+#include "bilinear_interpolation.h"
+#include "half_weighting.h"
 #include <utility>
 
 using std::pair;
 
-class multigrid : solver_basic
+class multigrid
 {
 public:
   // constants
 
+  const static int DEF_BASE = 5;
+
   // constructors
-  multigrid(int max, solver& smoother);	// run for at most max iterations
+  /* run for at most 'iter' iterations, starting on a square grid with 'base'
+   * points to a side, using 'smoother' to smooth at each step.
+   */
+  multigrid(int iter, int base, solver& smoother);
   multigrid();	// any number of iterations, using gauss-seidel to smooth
 
   // methods
 
   // solve and return number of iters.
-  int solve(problem_resizable& prob, goal& g);
+  template<class ProblemResizable> int solve(ProblemResizable& prob, goal& g);
   // synonym for solve
-  int operator()(problem_resizable& prob, goal& g);
+  template<class ProblemResizable> int operator()
+    (ProblemResizable& prob, goal& g);
 
 private:
+  int co;
+  int mg;		// size of the smallest grid
   gauss_seidel def_smooth;
   solver& smooth;	// smoother
 
-  void prolong(problem& coarse, problem& fine);
-  void restrict(problem& fine, problem& coarse);
 };
+
+// recursive templated version
+template<class ProblemResizable> int multigrid::solve(ProblemResizable& prob, goal& g)
+{
+  bilinear_interpolation bi;
+  half_weighting hw;
+  int iter = 0;
+
+  if(prob.I() == mg){
+    iter = smooth(prob, g);
+  }
+  else{
+    ProblemResizable orig = prob.clone();
+    prob.shrink(hw);
+    iter = (*this).solve(prob, g);		// recurse
+    bi(prob, orig);
+    iter += smooth(orig, g);	// postsmoothing
+    prob = orig;
+  }
+  return iter;
+}
+
+template<class ProblemResizable> int multigrid::operator()
+  (ProblemResizable& prob, goal& g)
+{ return solve(prob, g); }
