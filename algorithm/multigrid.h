3f8c22c8016c7113fe1679eaa1915b783ae8ad95
Waylon Flinn <waylonflinn@users.noreply.github.com>
2004-09-15 12:32:49 +0000

    changed multigrid class back to the simpler version from r111.

--- algorithm/multigrid.h
+++ algorithm/multigrid.h
@@ -37,7 +37,6 @@ public:
     (ProblemResizable& prob, goal& g);
 
 private:
-  bool init;
   int co;
   int mg;		// size of the smallest grid
   gauss_seidel def_smooth;
@@ -45,46 +44,23 @@ private:
 
 };
 
-/* recursive templated version
- * requires operator+=(), operator-=(), shrink(), and clone()
- */
+// recursive templated version
 template<class ProblemResizable> int multigrid::solve(ProblemResizable& prob, goal& g)
 {
   bilinear_interpolation bi;
   half_weighting hw;
-  int cyc = 0;
-  int m_cyc = 2;
   int iter = 0;
 
   if(prob.I() == mg){
     iter = smooth(prob, g);
-    (*this).init = true;
   }
   else{
     ProblemResizable orig = prob.clone();
-    ProblemResizable diff = prob.clone();
-    ProblemResizable shrunk;
-    while((cyc < m_cyc) && !g(prob)){
-      if(init){
-	iter += smooth(orig, g);	// presmooting
-	prob.shrink(hw);
-	shrunk = prob.clone();
-	iter += (*this).solve(prob, g);	// recurse
-	shrunk -= prob;
-	bi(shrunk, diff);
-	orig -= diff;
-	iter += smooth(orig, g);		// postsmoothing
-	prob = orig;
-      }
-      else{
-	prob.shrink(hw);
-	iter += (*this).solve(prob, g);	// recurse
-	bi(prob, orig);
-	iter += smooth(orig, g);		// postsmoothing
-	prob = orig;
-      }
-      ++cyc;
-    }
+    prob.shrink(hw);
+    iter = (*this).solve(prob, g);		// recurse
+    bi(prob, orig);
+    iter += smooth(orig, g);	// postsmoothing
+    prob = orig;
   }
   return iter;
 }
