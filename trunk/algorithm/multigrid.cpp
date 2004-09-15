05958ea5c70250e4a9365942feceeac666e3e3e9
ewf <ewf@339b2cbf-344d-0410-bec1-b5a737de3891>
2004-09-15 12:32:49 +0000

    changed multigrid class back to the simpler version from r111.

--- trunk/algorithm/multigrid.cpp
+++ trunk/algorithm/multigrid.cpp
@@ -2,8 +2,8 @@
 #include "multigrid.h"
 
 multigrid::multigrid(int max, int base, solver& smoother) : 
-  init(false), co(max), mg(base), smooth(smoother) {}
+  co(max), mg(base), smooth(smoother) {}
 
 multigrid::multigrid() :
-  init(false), co(std::numeric_limits<int>::max()), mg(multigrid::DEF_BASE),
+  co(std::numeric_limits<int>::max()), mg(multigrid::DEF_BASE),
   def_smooth(1000), smooth(def_smooth) {}
