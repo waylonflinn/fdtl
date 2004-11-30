0349ae94883787ae56602c14d3af17797772fdcf
Waylon Flinn <waylonflinn@users.noreply.github.com>
2004-11-30 18:15:20 +0000

    added implementation for the transform 'jacobian_cyl'.

--- transform/jacobian_cyl.cpp
+++ transform/jacobian_cyl.cpp
@@ -1,6 +1,13 @@
-#include <jacobian_cyl.h>
+#include "jacobian_cyl.h"
 
 template <class StaticSolution>
-jacobian_cyl::jacobain_cyl(StaticSolution solution)
+jacobian_cyl<StaticSolution>::jacobian_cyl<StaticSolution>(const StaticSolution& solution)
 {
+  sol=solution;
+}
+
+template <class StaticSolution>
+double jacobian_cyl<StaticSolution>::at(int i, int j)
+{
+  return sol.at(i,j)*2*M_PI*(sol.x0()+(sol.dx()*i));
 }
--- transform/jacobian_cyl.h
+++ transform/jacobian_cyl.h
@@ -1,13 +1,26 @@
 /* a transformation resulting from the multiplication of a problem
- * by the jacobian for cylidrical coordinates (r*sin(theta)).
+ * by the jacobian for cylidrical coordinates (r*sin(theta)), where
+ * the problem has cylindrical (theta) symmetry and the jacobian becomes
+ * r*2*PI (PI here *is* the ubiquitious constant relating the diameter of
+ * a circle to it's circumference).
+ * the template type for this class is the Static Solution which will be
+ * transformed.
  */ 
 
-template <class StaticSolution>
+#include <cmath>
+
+template<class StaticSolution>
 class jacobian_cyl {
 
  public:
-  jacobian_cyl(StaticSolution solution);
+  jacobian_cyl(const StaticSolution& solution);
 
+  int I() const {return sol.I();}
+  int J() const {return sol.J();}
+  double dx() const {return sol.dx();}
+  double dy() const {return sol.dy();}
+  double x0() const {return sol.x0();}
+  double y0() const {return sol.y0();}
   double at(int i, int j);
 
  private:
